b51a57ccb16cb39d1c04b6ce0fe56e348a49d6e4
JayWelborn <jesse.welborn@gmail.com>
2017-03-14 08:33:19 +0100

    animated blog expansion

--- chamberlin/home/static/home/js/home.js
+++ chamberlin/home/static/home/js/home.js
@@ -1,9 +1,20 @@
-var jumboHeight = $('.jumbotron').outerHeight();
-function parallax(){
-    var scrolled = $(window).scrollTop();
-    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
-}
+$(document).ready(function(){
+    // set height of jumbotron
+    var jumboHeight = $('.jumbotron').outerHeight();
+    // change height of bg as document is scrolled
+    function parallax(){
+        var scrolled = $(window).scrollTop();
+        $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+    }
 
-$(window).scroll(function(e){
-    parallax();
+    $(window).scroll(function(e){
+        parallax();
+    });
+
+    $('.blog .button').click(function(){
+        $( '.blog' ).toggleClass( "expanded" );
+        $( this ).text(function(i, text){
+            return text === "Read More" ? "Read Less" : "Read More";
+        })
+    });
 });
